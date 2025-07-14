df9bd1424ea8734f343c873bb8c3cfdaee9480d5
sir charles onyango <charlesotieno404@gmail.com>
2025-07-14 13:46:27 +0300

    Calculated simple interest using basic python arithmetic operations.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000 # Representing $1000
+rate = 0.05 # Rep 5% annual interest rate
+time = 3 # Rep 3 years
+interest = principal * rate * time
+print("The simple interest is:", interest)
