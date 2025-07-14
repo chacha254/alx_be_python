92b70bc76bc0320bf44118405c4d0d7eaae43839
sir charles onyango <charlesotieno404@gmail.com>
2025-07-14 14:49:15 +0300

    Calculated projected savings for one year using variables and simple arithmetc expressions in python

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses
+print("Your monthly savings are", monthly_savings)
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print("Projected savings after one year, with interest, is:", annual_savings)
+
