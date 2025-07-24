08101c93b84407baa69041208cc9d34eb000e14a
sir charles onyango <charlesotieno404@gmail.com>
2025-07-24 13:30:40 +0300

    Wrote a program that gives weather advice based on user input

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather = input("Whats the weather like today?(sunny/rainy/cold)")
+
+if weather == "sunny":
+    print("Wear a t-shirt and sun glasses.")  
+elif weather == "rainy":
+    print("Dont forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, i dont have reccomendation for this weather.")
