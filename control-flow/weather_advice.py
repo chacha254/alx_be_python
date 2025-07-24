ebf03c01b2db0ba4e9492619c411798261f718d5
sir charles onyango <charlesotieno404@gmail.com>
2025-07-24 13:34:20 +0300

    Added some correction to the grammar

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-weather = input("Whats the weather like today?(sunny/rainy/cold)")
+weather = input("What's the weather like today?(sunny/rainy/cold):")
 
 if weather == "sunny":
-    print("Wear a t-shirt and sun glasses.")  
+    print("Wear a t-shirt and sunglasses.")  
 elif weather == "rainy":
-    print("Dont forget your umbrella and a raincoat.")
+    print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, i dont have reccomendation for this weather.")
+    print("Sorry, i don't have reccomendation for this weather.")
