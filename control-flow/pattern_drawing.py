6ea5a2727e89da2b4908965a7707a181e2dad41b
sir charles onyango <charlesotieno404@gmail.com>
2025-07-26 13:35:25 +0300

    Pattern creation.

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+size = int(input("Enter the size of the pattern"))  #prompt user for pattern size\]
+if size <= 0:
+    print("Enter a positive interger")
+else:
+    sizes = 0 
+
+    while sizes < size:
+        for col in range(size):
+            print("*", end="")
+        print()
+        sizes += 1
