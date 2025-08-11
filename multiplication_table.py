75b8773f41d3a4fb7de009394ddc4fd3dd587325
sir charles onyango <charlesotieno404@gmail.com>
2025-08-12 00:04:09 +0300

    ...

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number to see its multiplication table:"))
 numbers = range(1, 11)
 
-for product in numbers:
+for product in numbers: #for loop iterating from 1 to 10
     print(f"{number} * {product} = {number * product}")
     product += 1
